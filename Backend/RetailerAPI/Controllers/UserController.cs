8d94607c6e8ab40efc5b517da94d77e7e8514df6
ndivhuho22 <54325790+ndivhuho22@users.noreply.github.com>
2021-09-16 10:02:38 +0200

    User&UserTypeController

---
+++ Backend/RetailerAPI/Controllers/UserController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RetailAPI.DataAccess.DataAccess;
+using RetailAPI.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ProjectDbContext _context;
+
+
+        public UserController(IConfiguration config, ProjectDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+
+        //This fmethod will get all Users records
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+
+        private bool userTypeExists(int id)
+        {
+            return _context.Users.Any(e => e.UserID == id);
+        }
+
+
+        //This method will get Users records using ID
+        //[Route("api/people/GetUsers")]
+        [HttpGet("{UserTypeId}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        //This method will PUT or Update User records
+        //[Route("api/PutUsers")]
+        [HttpPut("{UserTID}")]
+        public async Task<IActionResult> PutUsers(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (user == null)
+                //if(!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        //Add new Users
+        [HttpPost]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
+
+        }
+
+
+        //This method will DELETE User records
+        [Route("api/people/DeleteUsers")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
