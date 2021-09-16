892b32dd4e7cf212d5bbabab449fd0879cdbe4bb
katlegoRakgalakane <88768208+katlegoRakgalakane@users.noreply.github.com>
2021-09-16 14:07:31 +0200

    candidate and assessment booking controllers

---
+++ Backend/RetailerAPI/Controllers/AssessmentBookingDetailController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RetailAPI.DataAccess.DataAccess;
+using RetailAPI.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssessmentBookingDetailController : ControllerBase
+    {
+
+        //Bring in the configurtion file
+        private readonly IConfiguration _config;
+        private readonly ProjectDbContext _context;
+        //Overload constructor with configuration
+        public AssessmentBookingDetailController(IConfiguration config, ProjectDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+        //Create Http methods aka action methods
+
+        /*
+         * This method will get all assessmentbooking details records
+         * Route api/Assessment/GetAllAssessments
+         */
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AssessmentBookingDetail>>> Get()
+        {
+            return await _context.AssessmentBookingDetails.ToListAsync();
+        }
+
+        //Get existing Assesmentbookingdetails by ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AssessmentBookingDetail>> AssessmentBookingDetailByID(int id)
+        {
+            var AssessmentBookingDetail = await _context.AssessmentBookingDetails.FindAsync(id);
+
+            if (AssessmentBookingDetail == null)
+            {
+                return NotFound();
+            }
+
+            return AssessmentBookingDetail;
+        }
+        //Add new AssessmentBookingDetail
+        [HttpPost]
+        public async Task<ActionResult<AssessmentBookingDetail>> AddAssessmentBookingDetail(AssessmentBookingDetail assessmentBookingDetail)
+        {
+            _context.AssessmentBookingDetails.Add(assessmentBookingDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAssessmentById", new { id = assessmentBookingDetail.DetailID }, assessmentBookingDetail);
+
+        }
+
+        //UPDATE
+        // PUT: api/Assessment/UpdateAssessmentDetail
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAssessmentBookingDetail(int id, AssessmentBookingDetail assessmentBookingDetail)
+        {
+            assessmentBookingDetail.DetailID = id;
+
+            _context.Entry(assessmentBookingDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (AssessmentBookingDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //Check for existance
+        private bool AssessmentBookingDetailExists(int id)
+        {
+            return _context.AssessmentBookingDetails.Any(e => e.DetailID == id);
+        }
+
+        //DELETE
+        // DELETE: api/DCandidates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AssessmentBookingDetail>> DeleteAssessmentBookingDetail(int id)
+        {
+            var assessmentBookingDetail = await _context.AssessmentBookingDetails.FindAsync(id);
+            if (assessmentBookingDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.AssessmentBookingDetails.Remove(assessmentBookingDetail);
+            await _context.SaveChangesAsync();
+
+            return assessmentBookingDetail;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Backend/RetailerAPI/Controllers/CandidateController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RetailAPI.DataAccess.DataAccess;
+using RetailAPI.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        //Bring in the configurtion file
+        private readonly IConfiguration _config;
+        private readonly ProjectDbContext _context;
+        //Overload constructor with configuration
+        public CandidateController(IConfiguration config, ProjectDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+        //Create Http methods aka action methods
+
+        /*
+         * This method will get all assessment records
+         * Route api/Assessment/GetAllAssessments
+         */
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Candidate>>> Get()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+
+        //Get existing Assesment by ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidate>> CandidateByID(int id)
+        {
+            var Candidate = await _context.Candidates.FindAsync(id);
+
+            if (Candidate == null)
+            {
+                return NotFound();
+            }
+
+            return Candidate;
+        }
+        //Add new Candidate
+        [HttpPost]
+        public async Task<ActionResult<Candidate>> AddCandidate(Candidate Candidate)
+        {
+            _context.Candidates.Add(Candidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCandidateById", new { id = Candidate.CandidateID }, Candidate);
+
+        }
+
+        //UPDATE
+        // PUT: api/Assessment/UpdateAssessment
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCandidate(int id, Candidate Candidate)
+        {
+            Candidate.CandidateID = id;
+
+            _context.Entry(Candidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (CandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool CandidateExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Check for existance
+        private bool CandidateExist(int id)
+        {
+            return _context.Candidates.Any(e => e.CandidateID == id);
+        }
+
+        //DELETE
+        // DELETE: api/DCandidates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Candidate>> DeleteCandidate(int id)
+        {
+            var Candidate = await _context.Candidates.FindAsync(id);
+            if (Candidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Candidates.Remove(Candidate);
+            await _context.SaveChangesAsync();
+
+            return Candidate;
+
+        }
+
+        }
+    }
